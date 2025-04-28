6ea6a7451586640e057fd099fade02fc9ace4132
Noah Galvez <ngalvez@factoriabiz.com>
2025-04-28 08:09:15 +0200

    Customize View Pages and add filters and tabs to table

---
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\EmployeeResource\Pages;
+
+use App\Filament\Resources\EmployeeResource;
+use App\Models\Employee;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab as ListRecordsTab;
+use Illuminate\Database\Eloquent\Builder;
+
+class ListEmployees extends ListRecords
+{
+    protected static string $resource = EmployeeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => ListRecordsTab::make(),
+            'This Week' => ListRecordsTab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_hired', '>=', now()->subWeek()))
+                ->badge(Employee::query()->where('date_hired', '>=', now()->subWeek())->count()),
+            'This Month' => ListRecordsTab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_hired', '>=', now()->subMonth()))
+                ->badge(Employee::query()->where('date_hired', '>=', now()->subMonth())->count()),
+            'This Year' => ListRecordsTab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_hired', '>=', now()->subYear()))
+                ->badge(Employee::query()->where('date_hired', '>=', now()->subYear())->count()),
+        ];
+    }
+}
